de93ffc296ec0d0e177fd3df9721621119570948
NikhilCyberk <knikhil128@gmail.com>
2023-03-27 16:01:52 +0530

    assignment 8 ooad

---
+++ ooad lab/27-03.2023/4.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+string Exception()
+{
+    return "string Error";
+}
+int32_t main()
+{
+    try
+    {
+        try
+        {
+            throw(Exception());
+        }
+        catch (int x)
+        {
+            cerr << "Integer Error";
+        }
+    }
+    catch (string s)
+    {
+        cout << "error : " << s;
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
